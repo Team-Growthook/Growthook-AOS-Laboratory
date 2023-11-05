6d6ffa5e37f2f5c5170936e4273a5a78555704f1
Cat-JiYoung <kimyamae82@gmail.com>
2023-11-06 05:31:59 +0900

    [FEAT] 바텀네비게이션 구현

---
+++ app/src/main/java/com/growthook/growthooklaboratory/presentation/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.growthook.growthooklaboratory.presentation
+
+import com.growthook.growthooklaboratory.R
+import com.growthook.growthooklaboratory.databinding.FragmentHomeBinding
+import com.growthook.growthooklaboratory.util.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home)
