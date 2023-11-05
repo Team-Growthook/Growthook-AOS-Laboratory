e671bbde245ffbcc6fc234c0a9553ed58c194191
Cat-JiYoung <kimyamae82@gmail.com>
2023-11-06 05:30:58 +0900

    [CHORE] gradle, library version 수정 / gitIgnore 수정

--- app/src/main/java/com/growthook/growthooklaboratory/data/service/ExampleService.kt
+++ app/src/main/java/com/growthook/growthooklaboratory/data/service/ExampleService.kt
@@ -2,13 +2,11 @@ package com.growthook.growthooklaboratory.data.service
 
 import com.growthook.growthooklaboratory.data.model.request.ExampleRequest
 import com.growthook.growthooklaboratory.data.model.response.ExampleResponse
-import retrofit2.http.Body
-import retrofit2.http.POST
 
 interface ExampleService {
     // 예시 API
-    @POST("api/example")
+//    @POST("api/example")
     suspend fun postExample(
-        @Body request: ExampleRequest,
+        request: ExampleRequest,
     ): ExampleResponse
 }
