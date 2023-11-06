a610196ace4cab32602f6bd9036c2e570b1dbb53
Cat-JiYoung <kimyamae82@gmail.com>
2023-11-06 16:05:50 +0900

    [FEAT] 바텀네비게이션 위에 겹치는 바텀시트 구현

--- app/src/main/java/com/growthook/growthooklaboratory/presentation/HomeFragment.kt
+++ app/src/main/java/com/growthook/growthooklaboratory/presentation/HomeFragment.kt
@@ -1,7 +1,21 @@
 package com.growthook.growthooklaboratory.presentation
 
+import android.os.Bundle
+import android.view.View
 import com.growthook.growthooklaboratory.R
 import com.growthook.growthooklaboratory.databinding.FragmentHomeBinding
 import com.growthook.growthooklaboratory.util.base.BaseFragment
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home)
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clickListner()
+    }
+
+    private fun clickListner() {
+        binding.viewHomeMain.setOnClickListener {
+            val bottomSheetDialog = HomeBottomsheetFragment()
+            bottomSheetDialog.show(parentFragmentManager, "show")
+        }
+    }
+}
